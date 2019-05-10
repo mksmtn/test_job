1bf7977d2ad96b232230b57a002eec461910eb92
Maxim Matyunin <mksmtn@tutanota.com>
2019-05-10 20:24:31 +0400

    Hold variables in Config class instance

--- main.py
+++ main.py
@@ -2,24 +2,50 @@ import numpy as np
 import pandas as pd
 
 
-def generate_csv(file: str, nrows: int, ncols: int):
-    """Generate CSV file with random numbers
+class Config:
+    """Holds some project-related variables"""
 
-    :param file: Name of CSV file to write numbers
-    :param nrows: Number of rows in the generated CSV file
-    :param ncols: Number of columns in the generated CSV file
-    """
+    def __init__(self, file: str, nrows: int, ncols: int):
+        """
 
-    array = np.random.rand(nrows, ncols)
+        :param file: Name of CSV file to write numbers
+        :param nrows: Number of rows in the generated CSV file
+        :param ncols: Number of columns in the generated CSV file
+        """
+        self._file = file
+        self._nrows = nrows
+        self._ncols = ncols
+
+    @property
+    def file(self) -> str:
+        return self._file
+
+    @property
+    def nrows(self) -> int:
+        return self._nrows
+
+    @property
+    def ncols(self) -> int:
+        return self._ncols
+
+
+def generate_csv(config: Config):
+    """Generate CSV file with random numbers"""
+    array = np.random.rand(config.nrows, config.ncols)
     df = pd.DataFrame(array)
-    df.to_csv(file)
+    df.to_csv(config.file)
 
 
-def main():
-    # generate_csv()
+def main(config: Config):
+    generate_csv(config)
     # TODO
     pass
 
 
 if __name__ == '__main__':
-    main()
+    config = Config(
+        file='vectors.csv',
+        nrows=501,
+        ncols=11,
+    )
+    main(config)
