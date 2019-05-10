f7eb1bb3680b27f402308e30e86c33031e424c17
Maxim Matyunin <mksmtn@tutanota.com>
2019-05-10 20:49:46 +0400

    Add random sign to generaned numbers

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 
@@ -29,23 +31,39 @@ class Config:
         return self._ncols
 
 
-def generate_csv(config: Config):
-    """Generate CSV file with random numbers"""
-    array = np.random.rand(config.nrows, config.ncols)
-    df = pd.DataFrame(array)
-    df.to_csv(config.file)
+class Data:
+    """Creates or loads CSV data"""
+    def __init__(self, config: Config):
+        self._config = config
+
+        if os.path.isfile(config.file):
+            self._df = pd.read_csv(config.file)
+        else:
+            self._df = self._generate_csv()
+
+    def _generate_csv(self) -> pd.DataFrame:
+        """Generate CSV file with random numbers"""
+        array = np.random.rand(self._config.nrows, self._config.ncols)
+        signs = [-1, 1]
+
+        for row_number in range(self._config.nrows):
+            for col_number in range(self._config.ncols):
+                sign = np.random.choice(signs)
+                array[row_number][col_number] = sign * array[row_number][col_number]
+
+        df = pd.DataFrame(array)
+        df.to_csv(self._config.file)
+        return df
 
 
-def main(config: Config):
-    generate_csv(config)
-    # TODO
-    pass
-
-
-if __name__ == '__main__':
+def main():
     config = Config(
         file='vectors.csv',
         nrows=501,
         ncols=11,
     )
-    main(config)
+    data = Data(config)
+
+
+if __name__ == '__main__':
+    main()
