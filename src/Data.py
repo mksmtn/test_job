Fix min/max distance calculation

src/Data.py
@@ -1,5 +1,3 @@
-from os import path
-
 import numpy as np
 import pandas as pd
 
@@ -7,29 +5,46 @@ from src.Config import Config
 
 
 class Data:
-    """Creates or loads CSV data"""
-    def __init__(self, config: Config):
-        self._config = config
+    """Vectors' data from CSV file"""
 
-        if path.isfile(config.file):
-            self._df = pd.read_csv(config.file)
-        else:
-            self._df = self._generate_csv()
+    _bothProvidedMsg = 'Either `config` or `file` argument should be provided, but not both'
+    _noneProvidedMsg = 'Neither `config` nor `file` argument is provided'
 
-    @property
-    def data_frame(self) -> pd.DataFrame:
-        return self._df
-
-    def _generate_csv(self) -> pd.DataFrame:
+    @staticmethod
+    def generate_csv(config: Config) -> pd.DataFrame:
         """Generate CSV file with random numbers"""
-        array = np.random.rand(self._config.nrows, self._config.ncols)
+        array = np.random.rand(config.nrows, config.ncols)
         signs = [-1, 1]
 
-        for row_number in range(self._config.nrows):
-            for col_number in range(self._config.ncols):
+        for row_number in range(config.nrows):
+            for col_number in range(config.ncols):
                 sign = np.random.choice(signs)
                 array[row_number][col_number] = sign * array[row_number][col_number]
 
         df = pd.DataFrame(array)
-        df.to_csv(self._config.file)
+        df.to_csv(config.file, header=False, index=False)
         return df
+
+    def __init__(self, config: Config = None, file: str = None):
+        """Create or load CSV data
+
+        If `config` is provided, a new CSV file is created.
+        If `file` is provided, data gets loaded from that file.
+        If both are provided, `TypeError` is thrown.
+
+        :raises TypeError: if given both `config` and `file` arguments, or neither
+        """
+        if config and file:
+            raise TypeError(Data._bothProvidedMsg)
+
+        if not config and not file:
+            raise TypeError(Data._noneProvidedMsg)
+
+        if file:
+            self._df = pd.read_csv(file, header=None, index_col=False)
+        elif config:
+            self._df = Data.generate_csv(config)
+
+    @property
+    def data_frame(self) -> pd.DataFrame:
+        return self._df